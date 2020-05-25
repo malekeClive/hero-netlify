131227e7f64a5dea206bedb2ed8e92e75d07d467
clive <francoclivemaleke23@gmail.com>
2020-05-25 18:32:47 +0700

    fix bug 6

---
+++ build/precache-manifest.ea6c9c072a0495eed1f715e81f47594b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb82def84de285077e16d7aa7b3ccd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8802c6636da367f067a6",
+    "url": "/static/css/main.c4136b7f.chunk.css"
+  },
+  {
+    "revision": "0b4c89ef8a5f4047583f",
+    "url": "/static/js/2.6035824b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6035824b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8802c6636da367f067a6",
+    "url": "/static/js/main.908659c0.chunk.js"
+  },
+  {
+    "revision": "d94290d8055dd609c7eb",
+    "url": "/static/js/runtime-main.85d500f0.js"
+  }
+]);
